b48c9d59e5bfb0376adaf7d1fd004322d9d71cb0
adiafa-dev <th30f4n1@gmail.com>
2025-11-14 22:13:27 +0700

    update scroll button to top

--- components/ui/scrollToTopButton.tsx
+++ components/ui/scrollToTopButton.tsx
@@ -28,7 +28,7 @@ const ScrollToTopButton = () => {
     <button
       onClick={scrollToTop}
       aria-label='Scroll to top'
-      className={`bg-primary-200 fixed right-8 bottom-8 z-50 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full text-black shadow-[0_0_20px_rgba(163,230,53,0.5)] transition-all hover:shadow-[0_0_30px_rgba(163,230,53,0.7)] ${visible ? 'translate-y-0 opacity-100' : 'pointer-events-none translate-y-10 opacity-0'} `}
+      className={`bg-primary-200 fixed right-8 bottom-8 z-99 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full text-black shadow-[0_0_20px_rgba(163,230,53,0.5)] transition-all hover:shadow-[0_0_30px_rgba(163,230,53,0.7)] ${visible ? 'translate-y-0 opacity-100' : 'pointer-events-none translate-y-10 opacity-0'} `}
     >
       <ArrowUp size={22} />
     </button>
